12bffd9fda8cb11b3d7033169f9c264c38bb3da0
chaohony <1013415442@qq.com>
2018-05-09 09:01:00 +0800

    update

--- src/main.js
+++ src/main.js
@@ -1,1 +1,24 @@
 import "./styl/index.styl"
+window.onload = function() {
+  let h = document.createElement('div')
+  let f = document.createElement('ul')
+  let header = document.querySelector('.header')
+  let footer = document.querySelector('.footer')
+
+  h.innerHTML = `<nav class="nav"><ul>
+    <li class="collection"><a href="#">COLLECTION</a></li>
+    <li class=campaign"><a href="#">CAMPAIGN</a></li>
+    <li class="lookbook"><a href="#">LOOKBOOK</a</li>
+    <li class="brand-story"><a href="#">BRAND STORY</a></li>
+    <li class="projects"><a href="#">PROJECTS</a></li>
+    <li class="contact"><a href="#">CONTACT</a></li>
+    <li class="stores"><a href="#">STORES</a></li>
+    </ul></nav>`
+
+  f.innerHTML = `<ul class="f"><li>@ 2018 Pomme de terre</li><li>INSTAGRAM</li><li>WEIBO</li><li>WECHAT</li></ul>`
+
+  header.appendChild(h)
+
+  footer.appendChild(f)
+}
+
