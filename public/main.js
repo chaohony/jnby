1c335cf971cd1a04984b4d391e5702f73831f122
chaohony <1013415442@qq.com>
2018-06-13 10:28:29 +0800

    update

--- public/main.js
+++ public/main.js
@@ -74,13 +74,13 @@ __webpack_require__(1);
 
 window.onload = function () {
   var state = {};
-  var timer = void 0;
+  var timer = null;
   var h = document.createElement('div');
   var f = document.createElement('div');
   var header = document.querySelector('.header');
   var footer = document.querySelector('.footer');
-  h.innerHTML = '<nav class="nav">\n                  <ul class="ul">\n                    <li class="collection">\n                      <a href="./page2.html">COLLECTION</a>\n                    </li>\n                    <li class="campaign">\n                      <a href="./page3.html">CAMPAIGN</a>\n                    </li>\n                    <li class="lookbook">\n                      <a href="./page4.html">LOOKBOOK</a>\n                      <div class="lo">AW 18</div>\n                      <div class="lo">SS 18</div>\n                    </li>\n                    <li class="brand-story">\n                      <a href="./page5.html">BRAND STORY</a>\n                    </li>\n                    <li class="projects sp">\n                      <a href="./page6.html">PROJECTS</a>\n                      <div class="lo sp">\n                        <a href="./page7.html">IS THAT YOU</a>\n                        <div class="spHidden">\u5BFB\u4EBA\u542F\u4E8B</div>\n                      </div>\n                      <div class="lo">\n                        PARENT - CHILD CLUB<div>\u4EB2\u5B50\u4FF1\u4E50\u90E8</div>\n                      </div>\n                      <div class="lo sp">\n                        <a class="sp" href="./page8.html">IT\'S DIFFERENT BUT FUNNY</a>\n                        <div>\u4E0D\u4E00\u6837\u624D\u597D\u73A9</div>\n                      </div>\n                    </li>\n                    <li class="contact">\n                      <a href="./page9.html">CONTACT</a>\n                    </li>\n                    <li class="stores">\n                      <a href="#">STORES</a>\n                    </li></ul>\n                    <div class="ballon">\n                      <span class="one"></span>\n                      <span class="two"></span>\n                      <span class="three"></span>\n                      </div>\n                  </nav>';
-  f.innerHTML = '<ul class="f"><li>@ 2018 Pomme de terre</li><li>INSTAGRAM</li><li>WEIBO</li><li>WECHAT</li></ul>';
+  h.innerHTML = '<nav class="nav">\n                  <ul class="ul">\n                    <li class="collection">\n                      <a class="li-a" href="./page2.html">\n                        <span class="en">COLLECTION</span>\n                        <span class="ch">\u642D\u914D</span>  \n                      </a>\n                    </li>\n                    <li class="campaign">\n                      <a class="li-a" href="./page3.html">\n                        <span class="en">CAMPAIGN</span>\n                        <span class="ch">\u5F62\u8C61\u7247</span>\n                      </a>\n                    </li>\n                    <li class="lookbook">\n                      <a class="li-a sp" href="./page4.html">\n                        <span class="en">LOOKBOOK</span>\n                        <span class="ch">\u4EA7\u54C1\u56FE\u518C</span>\n                        <br>\n                        <div class="lo-wrapper">\n                          <div class="lo">AW 18</div>\n                          <div class="lo">SS 18</div>\n                        </div> \n                      </a>\n                    </li>\n                    <li class="brand-story">\n                      <a class="li-a" href="./page5.html">\n                        <span class="en">BRAND STORY</span>\n                        <span class="ch">\u54C1\u724C\u6545\u4E8B</span>\n                      </a>\n                    </li>\n                    <li class="projects">\n                      <a class="li-a sp" href="./page6.html">\n                        <span class="en">PROJECTS</span>\n                        <span class="ch">\u4E13\u9898</span>\n                        <br>\n                        <div class="lo-wrapper">\n                          <div class="lo jumpToPage7">\n                            <span class="inner-en">IS THAT YOU</span>\n                            <div class="los inner-ch">\u5BFB\u4EBA\u542F\u4E8B</div>\n                          </div>\n                          <div class="lo">\n                            <span class="inner-en">PARENT - CHILD CLUB</span>\n                            <div class="los inner-ch">\u4EB2\u5B50\u4FF1\u4E50\u90E8</div>\n                          </div>\n                          <div class="lo jumpToPage8">\n                            <span class="inner-en">IT\'S DIFFERENT BUT FUNNY</span>\n                            <div class="los inner-ch">\u4E0D\u4E00\u6837\u624D\u597D\u73A9</div>\n                          </div>\n                        </div>\n                      </a>\n                    </li>\n                    <li class="contact">\n                      <a class="li-a" href="./page9.html">\n                        <span class="en">CONTACT</span>\n                        <span class="ch">\u8054\u7CFB\u6211\u4EEC</span>\n                      </a>\n                    </li>\n                    <li class="stores">\n                      <a class="li-a" href="./index.html">\n                        <span class="en">STORES</span>\n                        <span class="ch">\u5E97\u94FA</span>\n                      </a>\n                    </li></ul>\n                    <div class="ballon">\n                      <span class="one"></span>\n                      <span class="two"></span>\n                      <span class="three"></span>\n                      </div>\n                  </nav>';
+  f.innerHTML = '<ul class="f">\n                    <li>@ 2018 Pomme de terre</li>\n                    <li>INSTAGRAM</li>\n                    <li>WEIBO</li>\n                    <li>WECHAT</li>\n                 </ul>';
   header.appendChild(h);
   footer.appendChild(f);
 
@@ -123,7 +123,7 @@ window.onload = function () {
     var timer = setInterval(function () {
       if (!d) return;
       d.className = toggleClass(d);
-    }, 3000);
+    }, 5000);
   })(document.getElementById('page1'));
 
   function sliderImg(c, duration, f) {
@@ -147,7 +147,7 @@ window.onload = function () {
       i++;
       if (i === 2) {
         c.style.transition = 'none';
-        c.style.transform = 'translate3d(0,0,0)';
+        c.style.transform = 'translate3d(0px,0px,0px)';
         i = 0;
       }
     });
@@ -162,10 +162,21 @@ window.onload = function () {
     v0.style.transform = 'translate3d(0,0,0)';
     v1.style.transform = 'translate3d(0,0,0)';
     sliderImg(v0, 5000, 'h');
-    sliderImg(v1, 5000, 'v');
+    sliderImg(v1, 5000, 'h');
   }
 
   prepareToRun();
+
+  document.getElementsByClassName('jumpToPage8')[0].onclick = function (e) {
+    e.stopPropagation();
+    e.preventDefault();
+    window.open('./page8.html', '_self');
+  };
+  document.getElementsByClassName('jumpToPage7')[0].onclick = function (e) {
+    e.stopPropagation();
+    e.preventDefault();
+    window.open('./page7.html', '_self');
+  };
 };
 
 /***/ }),
